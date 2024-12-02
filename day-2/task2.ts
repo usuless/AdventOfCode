cebbb511067c40a48ccf98b11dac0ed59ae429b2
usuless <61857692+usuless@users.noreply.github.com>
2024-12-02 19:37:34 +0100

    first task

--- day-2/task2.ts
+++ day-2/task2.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 
 let redNosedReactorData: string = ''
+let firstIteration: Array<string[]> = []
 
 const dataLocation:string = "./day-2/data.txt"
 
@@ -18,21 +19,30 @@ fs.readFile(dataLocation, (err, data) => {
       for (let i = 0; i < nums.length; i++) {
           if (Number(nums[i]) === Number(nums[i + 1])) {
               // takie same elementy
+              nums.slice(i + 1, 1)
+              firstIteration.push(nums)
               correctReportsCount--
               return
             } else if (Number(nums[0]) < Number(nums[1])){
                 // ascending
                 if (Number(nums[i]) + 3 < Number(nums[i + 1]) || Number(nums[i]) > Number(nums[i + 1])) {
+                    nums.splice(i, 1)
+                    firstIteration.push(nums)
                     correctReportsCount--
                     return
                 }
             } else if (Number(nums[0]) > Number(nums[1])) {
                 // descending
                 if (Number(nums[i]) - 3 > Number(nums[i +1]) || Number(nums[i]) < Number(nums[i + 1])) {
+                    nums.splice(i, 1)
+                    firstIteration.push(nums)
                     correctReportsCount --
                     return
                 }
-            } } 
+            } }
+            firstIteration.push(nums)
         });
 console.log(correctReportsCount)
+console.log(firstIteration)
 });
+
