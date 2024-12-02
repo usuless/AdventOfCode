08074524f25fd03df33cd58017e487738da91cc9
usuless <61857692+usuless@users.noreply.github.com>
2024-12-02 08:14:42 +0100

    day 1

---
+++ day-1/task1.ts
@@ -0,0 +1,39 @@
+import * as fs from "fs";
+
+let historianLocations: string = "";
+let totalDistance: number = 0;
+
+const arr1: number[] = [];
+const arr2: number[] = [];
+
+const dataLocation:string = "./day-1/data.txt"
+
+const sortNumbers = (a: number, b: number) => {
+  return a - b;
+}
+
+fs.readFile(dataLocation, (err, data) => {
+  if (err) throw err;
+  
+  historianLocations = data.toString();
+  
+  const separateLines = historianLocations.split("\r\n");
+  const separator: string = "   ";
+  
+  separateLines.forEach((line) => {
+    const nums = line.split(separator);
+    
+    arr1.push(Number(nums[0]));
+    arr2.push(Number(nums[1]));
+  });
+
+  arr1.sort(sortNumbers);
+  arr2.sort(sortNumbers);
+  
+  for (let i = 0; i < arr1.length; i++) {
+    const difference = Math.abs(arr1[i] - arr2[i]);
+    totalDistance += difference;
+  }
+  
+  console.log(totalDistance);
+});
---
+++ day-1/task2.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+
+let similarityScore: number = 0
+
+const arr1: number[] = [];
+
+const arr2Appearances: Record<number, number> = {};
+
+const dataLocation:string = "./day-1/data.txt"
+
+fs.readFile(dataLocation, (err, data) => {
+  if (err) throw err;
+  
+  const separateLines = data.toString().split("\r\n");
+  const separator: string = "   ";
+  
+  separateLines.forEach((line) => {
+    const nums = line.split(separator);
+    
+    arr1.push(Number(nums[0]));
+
+    const arr2Key = Number(nums[1]);
+
+    if(!arr2Appearances[arr2Key]) arr2Appearances[arr2Key] = 0;
+    arr2Appearances[arr2Key] += 1;
+});
+
+    for(let i = 0; i < arr1.length; i++) {
+        const appearanceCount = arr2Appearances[arr1[i]] ?? 0;
+
+        similarityScore += arr1[i] * appearanceCount  ;
+    }
+    
+    console.log(similarityScore)
+});
