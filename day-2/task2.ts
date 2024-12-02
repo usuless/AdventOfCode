f1dcb3595947dc4064e6aa4612b82cc5aab72725
usuless <61857692+usuless@users.noreply.github.com>
2024-12-02 20:53:14 +0100

    task 2

--- day-2/task2.ts
+++ day-2/task2.ts
@@ -1,7 +1,40 @@
 import * as fs from "fs";
 
 let redNosedReactorData: string = ''
-let firstIteration: Array<string[]> = []
+const dumbNums = [1,4,7,8,9]
+
+const isNumSafe = (nums: number[]):boolean => {
+    
+    for (let i = 1; i < nums.length; i++) {
+      const difference = Math.abs(nums[i] - nums[i-1])
+      const startOnGrow = nums[1] > nums[0]
+      const isStillOnGrow = nums[i] - nums[i - 1] > 0
+    
+      if (difference > 3 || difference === 0) { return false }
+      if ( startOnGrow !== isStillOnGrow ) {return false }
+      }
+      return true
+}
+
+const isNumeSafeDoubleCheck = (num: number[]) => {
+    if(isNumSafe(num)) return true
+
+    for (let i = 0; i < num.length; i++) {
+        if (isNumSafe(num.filter((_, j) => j !== i))) return true
+      }
+      return false
+}
+
+
+const numsChecker = (nums: number[][]): number => {
+    return nums.filter(isNumSafe).length
+}
+
+const sumOfDoubleChecked = (nums: number[][]): number => {
+    return nums.filter(isNumeSafeDoubleCheck).length
+}
+
+console.log(isNumSafe(dumbNums))
 
 const dataLocation:string = "./day-2/data.txt"
 
@@ -12,37 +45,9 @@ fs.readFile(dataLocation, (err, data) => {
   
   const separateLines = redNosedReactorData.split("\r\n");
   const separator: string = " ";
-  let correctReportsCount = separateLines.length
   
-  separateLines.forEach((line) => {
-      const nums = line.split(separator);
-      for (let i = 0; i < nums.length; i++) {
-          if (Number(nums[i]) === Number(nums[i + 1])) {
-              // takie same elementy
-              nums.slice(i + 1, 1)
-              firstIteration.push(nums)
-              correctReportsCount--
-              return
-            } else if (Number(nums[0]) < Number(nums[1])){
-                // ascending
-                if (Number(nums[i]) + 3 < Number(nums[i + 1]) || Number(nums[i]) > Number(nums[i + 1])) {
-                    nums.splice(i, 1)
-                    firstIteration.push(nums)
-                    correctReportsCount--
-                    return
-                }
-            } else if (Number(nums[0]) > Number(nums[1])) {
-                // descending
-                if (Number(nums[i]) - 3 > Number(nums[i +1]) || Number(nums[i]) < Number(nums[i + 1])) {
-                    nums.splice(i, 1)
-                    firstIteration.push(nums)
-                    correctReportsCount --
-                    return
-                }
-            } }
-            firstIteration.push(nums)
-        });
-console.log(correctReportsCount)
-console.log(firstIteration)
-});
-
+  const numsAsNumbers = separateLines.map(line => line.split(separator).map(Number))
+    
+  console.log(numsChecker(numsAsNumbers))
+  console.log(sumOfDoubleChecked(numsAsNumbers))
+})
