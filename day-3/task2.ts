59e35f844a8e60db615969f38d57c6063e61a77b
usuless <61857692+usuless@users.noreply.github.com>
2024-12-03 17:04:59 +0100

    task2

--- day-3/task2.ts
+++ day-3/task2.ts
@@ -3,7 +3,8 @@ import * as fs from 'fs';
 const dataLocation: string = './day-3/data.txt'
 let computerData: string = ''
 let score: number = 0
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const regex:RegExp = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
+let isGood:boolean = true
 
 const regexFunc = (mul: string) => {
     const arrayWithoutBraces = mul.slice(0, -1)
@@ -14,19 +15,39 @@ const regexFunc = (mul: string) => {
         if(numbers[i] === ',') {
             num1 = Number(numbers.slice(0, i))
             num2 = Number(numbers.slice(i + 1, numbers.length))
+            console.log(num1)
+            console.log(num2)
             score += (num1 * num2)
         }
     }
     console.log(score)
 }
-    
+
+const doAndDontAddon = (TableOfRules:RegExpMatchArray) => {
+    for (let i = 0; i < TableOfRules.length; i++) {
+        if (TableOfRules[i] === "do()") {
+            console.log(TableOfRules[i])
+            isGood = true
+        } else if (TableOfRules[i] === "don't()") {
+            console.log(TableOfRules[i])
+            isGood = false
+        } else {
+            if (isGood === true) {
+                regexFunc(TableOfRules[i])
+            }
+        } 
+    }
+}
+
     // read file
     fs.readFile(dataLocation, (err, data) => {
         if (err) throw err;
         
     computerData = data.toString();
     const found = computerData.match(regex)
-
-    found?.forEach((mul) => regexFunc(mul))
-    
+    console.log(found)
+    //@ts-ignore
+    doAndDontAddon(found)
 })
+
+// 44071268
