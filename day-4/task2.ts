6dbe28ecf9fa9005f64d508c37869d35a1c6853e
usuless <61857692+usuless@users.noreply.github.com>
2024-12-04 13:11:03 +0100

    day4 finito

--- day-4/task2.ts
+++ day-4/task2.ts
@@ -2,7 +2,29 @@ import * as fs from "fs"
 
 const dataLocation: string = "./day-4/data.txt"
 let wordList = ""
-let wordCount = 0
+let countOfMAS = 0
+
+const checkForMAS = (text: string[], arrayIndex: number, letterIndex: number) => {
+    if (text[arrayIndex - 1][letterIndex - 1] === "M" && text[arrayIndex + 1][letterIndex + 1] === "S") {
+       if (text[arrayIndex - 1][letterIndex + 1] === "M" && text[arrayIndex + 1][letterIndex - 1] === "S") {
+        countOfMAS++
+       }
+
+       if (text[arrayIndex - 1][letterIndex + 1] === "S" && text[arrayIndex + 1][letterIndex - 1] === "M") {
+        countOfMAS++
+       }
+    }
+
+    if (text[arrayIndex - 1][letterIndex - 1] === "S" && text[arrayIndex + 1][letterIndex + 1] === "M") {
+        if (text[arrayIndex - 1][letterIndex + 1] === "M" && text[arrayIndex + 1][letterIndex - 1] === "S") {
+            countOfMAS++
+        }
+    
+        if (text[arrayIndex - 1][letterIndex + 1] === "S" && text[arrayIndex + 1][letterIndex - 1] === "M") {
+            countOfMAS++
+        }
+    }
+}
 
 fs.readFile(dataLocation, (err,data) => {
     if (err) throw err;
@@ -11,7 +33,10 @@ fs.readFile(dataLocation, (err,data) => {
     const parsedWordList = wordList.split("\r\n")
     for(let array = 0; array < parsedWordList.length; array++) {
         for (let letter = 0; letter < parsedWordList[array].length; letter++) {
+            if ( letter > 0 && letter < (parsedWordList[array].length - 1) && array > 0 && array < (parsedWordList.length - 1) && parsedWordList[array][letter] === "A") {
+                checkForMAS(parsedWordList, array, letter)
+            }
         }
     }
-    console.log(wordCount)
+    console.log(countOfMAS)
 })
