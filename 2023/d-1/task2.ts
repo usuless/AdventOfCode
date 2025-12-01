86b0300303f2e8e0b59e180c57a368f17361426c
usuless <61857692+usuless@users.noreply.github.com>
2025-12-01 23:55:49 +0100

    AoC

--- 2023/d-1/task2.ts
+++ 2023/d-1/task2.ts
@@ -2,33 +2,51 @@ import * as fs from "fs";
 
 let total = 0;
 
-const dataLocation = "data.txt";
+const digitWords: string[] = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "zero",
+];
+
+const dataLocation = "2023/d-1/data.txt";
 
 fs.readFile(dataLocation, (err, data) => {
-    if (err) throw err;
-    const records = data.toString();
-    let splitRecords = records.split("\r\n");
-    splitRecords.forEach((record) => {
-        let firstNum: number | null = null;
-        let lastNum: number | null = null;
-        record.split("").forEach((sign) => {
-            if (!isNaN(Number(sign))) {
-                if (firstNum == null) {
-                    firstNum = Number(sign);
-                } else {
-                    lastNum = Number(sign);
-                }
-            }
-        });
-        if (lastNum == null) {
-            lastNum = firstNum;
+  if (err) throw err;
+  const records = data.toString();
+  let splitRecords = records.split("\r\n");
+  splitRecords.forEach((record) => {
+    let firstNum: number | null = null;
+    let lastNum: number | null = null;
+    record.split("").forEach((sign, index) => {
+      //   if (!isNaN(Number(sign))) {
+      //     if (firstNum == null) {
+      //       firstNum = Number(sign);
+      //       console.log(index);
+      //     } else {
+      //       lastNum = Number(sign);
+      //     }
+      //   }
+      for (let index = 0; index < digitWords.length; index++) {
+        if (record.includes(digitWords[index])) {
+          console.log();
         }
-
-        let finalNumber = Number(String(firstNum) + String(lastNum));
-        total += finalNumber;
-        console.log(finalNumber);
+      }
     });
-    console.log(total);
+    if (lastNum == null) {
+      lastNum = firstNum;
+    }
+
+    let finalNumber = Number(String(firstNum) + String(lastNum));
+    total += finalNumber;
+  });
+  console.log(total);
 });
 
 /*
---
+++ 2025/1-day/task2.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+
+const dataLocation = "2025/1-day/data.txt";
+
+let value = 50;
+let numberOfZeroes = 0;
+
+fs.readFile(dataLocation, (err, data) => {
+  if (err) throw err;
+  const input = data.toString().split("\r\n");
+  input.forEach((val) => {
+    const tempVal = Number(val.slice(1));
+    for (let index = 0; index < tempVal; index++) {
+      if (val.slice(0, 1) === "L") {
+        if (value === 0) {
+          value = 99;
+          continue;
+        }
+        value--;
+      } else {
+        if (value === 99) {
+          value = 0;
+          numberOfZeroes++;
+          continue;
+        }
+        value++;
+      }
+      if (value === 0) {
+        numberOfZeroes++;
+      }
+    }
+  });
+  console.log(numberOfZeroes);
+});
